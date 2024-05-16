2375075853dc425e80106d9249e66d40b39791e1
Arthur Ambalov <arthurambalov@gmail.com>
2024-05-16 17:32:54 +0000

    Add correct number of lists of space rows to the board of length appropriate for its size

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
-class Board:
-    def __init__(self, x, y, length, width):
+class GridSpace():
+    """
+    Class representing a single space in the board
+    """
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class Board:
+    def __init__(self, side, length, width):
+        self.side = side
         self.length = length
         self.width = width
 
         self.grid = []
+        for i in range(width):
+            new_row = []
+            for j in range(length):
+                new_space = GridSpace(i, j)
+                new_row.append(new_space)
+            self.grid.append(new_row)
 
 def begin():
     """
@@ -19,6 +32,7 @@ def begin():
             # changes e.g "jack tannen" or "JACK TANNEN" to just "Jack Tannen"
             inputed_name = inputed_name.title()
             print(f"\nWelcome aboard, Captain {inputed_name}.")
+            test_board = Board("player", 5, 5)
             break
         else:
             print("No name entered.")
