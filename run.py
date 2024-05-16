41412cbe829c4988340065fd721d306004e15a7d
Arthur Ambalov <arthurambalov@gmail.com>
2024-05-16 17:06:52 +0000

    Add begin function with welcome message and loredump

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def begin():
+    """
+    Initial sequence: welcome the player and ask for their name
+    """
+    print("- WELCOME TO VOIDSHIPS -\n\nIt is the far flung future.\nAdvanced stealth technology results in most battles being fought by invisible 'voidships' blind-firing into unknown space.\nYou are the commander of a pirate outfit, raiding imperial patrols for fortune and glory.")
+
+begin()
