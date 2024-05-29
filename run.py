c7f30299badf71240af48a32475a58a736a968fa
Arthur Ambalov <arthurambalov@gmail.com>
2024-05-29 19:11:28 +0000

    Add validation for existing username and password

--- run.py
+++ run.py
@@ -306,16 +306,25 @@ def match_password(given_username, given_password):
     return False
 
 def try_login():
+    """
+    Offer to log in to an existing account
+    Username and password must match
+    On failure, go back to asking for login/signup
+    """
     given_username = input("Please enter your username: \n")
     if username_exists(given_username):
         given_password = input("Please enter your password: \n")
         if match_password(given_username, given_password):
-
+            print("You're in!")
+            pre_battle()
     else:
         print("This username does not exist.")
         ask_account()
 
 def ask_account():
+    """
+    Ask for either login or signup
+    """
     print("Do you have an existing account?")
     response = input("Y/N: \n")
     if response == "Y":
