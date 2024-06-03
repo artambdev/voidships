02599a89f887717ed61324bc0a6a97eeeaab6d7f
Arthur Ambalov <arthurambalov@gmail.com>
2024-06-03 15:09:58 +0000

    Cleanup account management code

--- accounts.py
+++ accounts.py
@@ -15,12 +15,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("voidships_users")
 
+
 def add_user(username, password):
     """
     Adds a new user to the database
     """
     users = SHEET.worksheet("users")
     users.append_row([username, password])
+
 
 def username_exists(username):
     """
@@ -33,6 +35,7 @@ def username_exists(username):
         if existing == username:
             return True
     return False
+
 
 def match_password(given_username, given_password):
     """
@@ -51,6 +54,7 @@ def match_password(given_username, given_password):
         return True
     return False
 
+
 def try_login():
     """
     Offer to log in to an existing account
@@ -64,10 +68,11 @@ def try_login():
             print(Fore.GREEN + "\nYou're in!\n")
         else:
             print(Fore.RED + "Password incorrect.")
-            ask_account()  
+            ask_account()
     else:
         print(Fore.RED + "This username does not exist.")
         ask_account()
+
 
 def try_signup():
     """
@@ -75,28 +80,36 @@ def try_signup():
     Keep asking until a username is given that isn't already taken
     Both username and password must fit a given length as well
     """
-    print(Fore.YELLOW + "Please enter your desired username (at least 3 characters) and password (at least 5 characters).")
+    print(f"""
+    {Fore.YELLOW}Please enter your desired username (at least 3 characters)
+    and password (at least 5 characters).
+    """)
     while True:
         given_username = str(input(Fore.YELLOW + "Username: \n"))
         given_password = str(input(Fore.YELLOW + "Password: \n"))
         try:
             if len(given_username) < 3:
                 raise ValueError(
-                    f"Username is only {str(len(given_username))} characters, must be at least 3."
+                    f"""
+                    Username is only {str(len(given_username))} characters,
+                    must be at least 3.
+                    """
                 )
             if len(given_password) < 5:
                 raise ValueError(
-                    f"Password is only {str(len(given_password))} characters, must be at least 5."
+                    f"""Password is only {str(len(given_password))} characters,
+                    must be at least 5."""
                 )
             if username_exists(given_username):
                 raise ValueError(
                     f"This username is already taken."
                 )
             add_user(given_username, given_password)
-            print(Fore.GREEN + "\nYou're all signed up!\n")  
-            break    
+            print(Fore.GREEN + "\nYou're all signed up!\n")
+            break
         except ValueError as e:
             print(f"{Fore.RED}Error: {e}. Please try again.")
+
 
 def ask_account():
     """
